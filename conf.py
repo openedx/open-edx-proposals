d9a6ad8e5e0582a18aa47465171226c41ad5a213
Calen Pennington <cale@edx.org>
2016-03-31 11:35:33 -0400

    Make it easy to reference OEP status tickets
    ARCH-36

--- conf.py
+++ conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'sphinx.ext.extlinks',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -293,3 +294,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+extlinks = {
+    'oep-status': ('https://openedx.atlassian.net/browse/%s', 'OEP-')
+}
+
