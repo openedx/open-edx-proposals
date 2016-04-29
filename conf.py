4b5d32bdb5738f143b8724f1d5f45a88c6a1ee12
Calen Pennington <cale@edx.org>
2016-04-29 15:50:03 -0400

    Don't try and use githubpages extension (for RTD compatibility)

--- conf.py
+++ conf.py
@@ -32,7 +32,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
