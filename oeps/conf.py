bcfebbf4fcc40eebf1287f3867c373ced6a6195e
Feanil Patel <feanil@edx.org>
2021-10-01 16:00:21 -0400

    docs: Update OEP-1
    Add a new "Obsolete" status that can be assigned to OEPs that are no
    longer relevant but don't have a new OEP supersedeing them.
    OEP-7(Migrating to Python 3) is a good example of an OEP that would
    benefit from this status existing.

--- oeps/conf.py
+++ oeps/conf.py
@@ -35,7 +35,10 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
+    'sphinx.ext.graphviz',
 ]
+
+graphviz_output_format = 'svg'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
