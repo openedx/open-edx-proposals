4868aacae48c24aca05f512607b896f7bf6422d4
Feanil Patel <feanil@axim.org>
2023-06-27 11:37:24 -0400

    fix: Update to the new format for intersphinx mappings.
    The old format is deprecated and will not be supported in future
    versions.  Starting with sphinx 6.x, we see warnings for this so
    updating now to silence the warnings.

--- oeps/conf.py
+++ oeps/conf.py
@@ -338,5 +338,10 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    "python": (
+        'https://docs.python.org/',
+        None
+    ),
+}
 
